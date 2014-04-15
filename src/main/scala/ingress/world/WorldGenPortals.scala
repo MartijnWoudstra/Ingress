a36d43f50ffc68a03f967becd6763f60201bfa9b
Backes Thierry <backes.thierry@gmail.com>
2014-04-15 14:12:15 +0200

    Git Test

--- src/main/scala/ingress/world/WorldGenPortals.scala
+++ src/main/scala/ingress/world/WorldGenPortals.scala
@@ -14,6 +14,7 @@ class WorldGenPortals(block: Block) extends WorldGenerator {
   override def generate(world: World, random: Random, x: Int, y: Int, z: Int): Boolean = {
     for (l <- 0 until 64) {
       if (world.isAirBlock(x, y, z) && world.getBlock(x, y, z).canBlockStay(world, x, y, z) && world.canBlockSeeTheSky(x, y - 1, z) && !world.isAirBlock(x, y - 1, z)) {
+        world.setBlock(x, y-1, z, block)
         world.setBlock(x, y, z, block)
       }
     }
