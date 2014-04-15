1664323fd7e44b5c8a0e4f8556ce22e6ffc84a31
Backes Thierry <backes.thierry@gmail.com>
2014-04-15 14:33:04 +0200

    Portal Generator creates diffrent types of portals

--- src/main/scala/ingress/world/WorldGenPortals.scala
+++ src/main/scala/ingress/world/WorldGenPortals.scala
@@ -14,8 +14,23 @@ class WorldGenPortals(block: Block) extends WorldGenerator {
   override def generate(world: World, random: Random, x: Int, y: Int, z: Int): Boolean = {
     for (l <- 0 until 64) {
       if (world.isAirBlock(x, y, z) && world.getBlock(x, y, z).canBlockStay(world, x, y, z) && world.canBlockSeeTheSky(x, y - 1, z) && !world.isAirBlock(x, y - 1, z)) {
-        world.setBlock(x, y-1, z, block)
-        world.setBlock(x, y, z, block)
+
+        val r = scala.util.Random
+        val i = r.nextInt(3) // [0;value[ from integers
+
+        i match{
+          case 0 =>
+            world.setBlock(x, y-1, z, block)
+
+          case 1 =>
+            world.setBlock(x, y-1, z, block)
+            world.setBlock(x, y, z, block)
+
+          case 2 =>
+            world.setBlock(x, y-1, z, block)
+            world.setBlock(x, y, z, block)
+            world.setBlock(x, y+1, z, block)
+        }
       }
     }
     true
